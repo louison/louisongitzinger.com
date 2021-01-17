32eeace40b1c5eea9695bc8e818cbbf5e18a9f7f
Louison Gitzinger <gitzingerlouison@gmail.com>
2021-01-17 13:22:11 +0100

    solve ts errors

---
+++ components/MDXComponents.tsx
@@ -0,0 +1,4 @@
+const MDXComponents = {
+};
+
+export default MDXComponents;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import '../styles/global.css';
 
 import type {AppProps /*, AppContext */} from 'next/app'
-import SEO from '../next-seo.config.js';
+import SEO from '../next-seo.config';
+import { MDXProvider } from '@mdx-js/react';
+import MDXComponents from '../components/MDXComponents';
 import {DefaultSeo} from 'next-seo';
-import {MDXProvider} from '@mdx-js/react';
 import Head from "next/head";
 
 function App({Component, pageProps}: AppProps) {
     return (
-        <MDXProvider>
+        <MDXProvider components={MDXComponents}>
             <Head>
                 <meta content="width=device-width, initial-scale=1" name="viewport"/>
             </Head>
